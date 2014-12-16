6fd8a55648618b76422b56aa735372642b3de1ac
MehdiChouag <mehdi.chouag@epitech.eu>
2014-12-16 14:01:16 +0100

    Patch Bug

--- bin/caesar.js
+++ bin/caesar.js
@@ -6,32 +6,31 @@ var program = require('commander');
 
 /* Setting commander and the options */
 program
-  .usage("[option] <file...>")
-  .option("-d <n>", "[number] [string...]")
-  .parse(process.argv);
+.usage("[option] <file...>")
+.option("-d <n>", "[number] [string...]")
+.parse(process.argv);
 
 /* handle the -d option and check if the argument is valid and is the string to encode exist */
 program
-	.on("-d", function (number){
-	  	var display = [];
-	  	var string = program.args;
-		if (number && program.args.length >= 1) {
-			for (var i = 0; i <= string.length - 1; i++) {
-				display.push(CaesarCrypto(number, string[i]));
-			}
-			display.forEach(function (str){
-				console.log(str);
-			});
+.on("-d", function (number){
+	var display = [];
+	var string = program.args;
+	if (number && program.args.length >= 1) {
+		for (var i = 0; i <= string.length - 1; i++) {
+			display.push(CaesarCrypto(number, string[i]));
 		}
-		else
-			console.log ("Usage : bin/caesar.js -d [number] [string...]");
-	})
-	.parse(process.argv);
+		display.forEach(function (str){
+			console.log(str);
+		});
+	}
+	else
+		console.log ("Usage : bin/caesar.js -d [number] [string...]");
+})
+.parse(process.argv);
 
 
 /* This function return the string encode or an error and takes 
 an argument the number for the shift and the string to encode */ 
-
 function CaesarCrypto(number, string){
 	if (isNaN(number)){
 		return "The shift for the caesar cryptography is not a number"
@@ -45,11 +44,20 @@ function CaesarCrypto(number, string){
 		for (var i = 0;  i <= string.length - 1; i++) {
 			var asciiCode = string.charCodeAt(i);
 			if (asciiCode >= 65 && asciiCode <= 90 || asciiCode >= 97 && asciiCode <= 122) {
-				if (asciiCode >= 65 && asciiCode <= 90 && asciiCode + number > 90 && number > 0)
-					asciiCode = 64;
-				else if (asciiCode >= 97 && asciiCode <= 122 && asciiCode + number > 122 && number > 0)
-					asciiCode = 96;
-				ret += String.fromCharCode(asciiCode + number % 26);
+				if (number % 26 != 0){
+					number %= 26;
+					if (asciiCode >= 97 && asciiCode <= 122 && (asciiCode + number > 122 || asciiCode + number < 97)) {
+						asciiCode = (asciiCode + number > 122) ? (96 + asciiCode + number - 122) : (122 + asciiCode + number - 96);
+						ret += String.fromCharCode(asciiCode);
+					} else if (asciiCode >= 65 && asciiCode <= 90 && (asciiCode + number > 90 || asciiCode + number < 65)){
+						asciiCode = (asciiCode + number > 90) ? (64 + asciiCode + number - 90) : (90 + asciiCode + number - 64);
+						ret += String.fromCharCode(asciiCode);
+					} else {
+						ret += String.fromCharCode(asciiCode + number);
+					}
+				}
+				else
+					ret += String.fromCharCode(asciiCode);
 			}
 			else
 				ret += string.charAt(i);
@@ -57,3 +65,5 @@ function CaesarCrypto(number, string){
 		return ret;
 	}
 }
+
+module.exports = CaesarCrypto;
